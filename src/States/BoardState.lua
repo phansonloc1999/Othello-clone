b3f70989d9552f74b5d8aa442bef5b34373bb352
phansonloc1999 <phansonloc1999@gmail.com>
2019-06-10 22:43:38 +0700

    Put all buttons into a table

--- src/States/BoardState.lua
+++ src/States/BoardState.lua
@@ -5,31 +5,34 @@ local SIZE_BUTTON_WIDTH, SIZE_BUTTON_BOX = 80, 80
 local BACK_BUTTON_WIDTH, BACK_BUTTON_HEIGHT = 100, 30
 
 function BoardState:init()
-    self._smallButton =
-        RectButton(
-        WINDOW_WIDTH / 2 - 3 * SIZE_BUTTON_WIDTH / 2,
-        WINDOW_HEIGHT / 2 - 3 * SIZE_BUTTON_BOX / 2,
-        SIZE_BUTTON_WIDTH,
-        SIZE_BUTTON_BOX,
-        "Small"
-    )
+    ---@type RectButton[]
+    self._buttons = {
+        ["small"] = RectButton(
+            WINDOW_WIDTH / 2 - 3 * SIZE_BUTTON_WIDTH / 2,
+            WINDOW_HEIGHT / 2 - 3 * SIZE_BUTTON_BOX / 2,
+            SIZE_BUTTON_WIDTH,
+            SIZE_BUTTON_BOX,
+            "Small"
+        )
+    }
 
-    local x, y = getPositionsWithOffsets(self._smallButton._boxX, self._smallButton._boxY, SIZE_BUTTON_WIDTH * 2, 0)
-    self._mediumButton = RectButton(x, y, SIZE_BUTTON_WIDTH, SIZE_BUTTON_BOX, "Medium")
+    local x, y =
+        getPositionsWithOffsets(self._buttons["small"]._boxX, self._buttons["small"]._boxY, SIZE_BUTTON_WIDTH * 2, 0)
+    self._buttons["medium"] = RectButton(x, y, SIZE_BUTTON_WIDTH, SIZE_BUTTON_BOX, "Medium")
 
-    x, y = getPositionsWithOffsets(self._smallButton._boxX, self._smallButton._boxY, 0, SIZE_BUTTON_BOX * 2)
-    self._largeButton = RectButton(x, y, SIZE_BUTTON_WIDTH, SIZE_BUTTON_BOX, "Large")
+    x, y = getPositionsWithOffsets(self._buttons["small"]._boxX, self._buttons["small"]._boxY, 0, SIZE_BUTTON_BOX * 2)
+    self._buttons["large"] = RectButton(x, y, SIZE_BUTTON_WIDTH, SIZE_BUTTON_BOX, "Large")
 
     x, y =
         getPositionsWithOffsets(
-        self._smallButton._boxX,
-        self._smallButton._boxY,
+        self._buttons["small"]._boxX,
+        self._buttons["small"]._boxY,
         SIZE_BUTTON_WIDTH * 2,
         SIZE_BUTTON_BOX * 2
     )
-    self._customButton = RectButton(x, y, SIZE_BUTTON_WIDTH, SIZE_BUTTON_BOX, "Custom")
+    self._buttons["custom"] = RectButton(x, y, SIZE_BUTTON_WIDTH, SIZE_BUTTON_BOX, "Custom")
 
-    self._backButton =
+    self._buttons["back"] =
         RectButton(
         WINDOW_WIDTH / 2 - BACK_BUTTON_WIDTH / 2,
         WINDOW_HEIGHT - 80,
@@ -40,15 +43,13 @@ function BoardState:init()
 end
 
 function BoardState:render()
-    self._smallButton:render()
-    self._mediumButton:render()
-    self._largeButton:render()
-    self._customButton:render()
-    self._backButton:render()
+    for k, v in pairs(self._buttons) do
+        self._buttons[k]:render()
+    end
 end
 
 function BoardState:update(dt)
-    if (self._backButton:collidesWithMouse() and love.mouse.wasPressed(1)) then
+    if (self._buttons["back"]:collidesWithMouse() and love.mouse.wasPressed(1)) then
         gStateMachine:change("menu")
     end
 end
