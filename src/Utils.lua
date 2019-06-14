5b3634e43bb39fca36ab4747f6ef8351c713f981
phansonloc1999 <phansonloc1999@gmail.com>
2019-06-14 20:55:34 +0700

    Add belongsTo func

--- src/Utils.lua
+++ src/Utils.lua
@@ -13,3 +13,17 @@ end
 function love.mouse.wasPressed(button)
     return love.mouse.buttonsPressed[button]
 end
+
+function belongsTo(parentTable, subTable)
+    for i = 1, #parentTable do
+        for j = 1, #parentTable[i] do
+            if (parentTable[i][j] ~= subTable[j]) then
+                break
+            end
+            if (j == 2) then
+                return true
+            end
+        end
+    end
+    return false
+end
